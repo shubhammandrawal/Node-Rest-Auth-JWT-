45231b26dbe71625be4ebe4fdf0c8d43ae22ed27
shubhammandrawal <Shubham@7785>
2023-03-23 21:04:28 +0530

    First commit

---
+++ config/db.js
@@ -0,0 +1,5 @@
+module.exports ={
+    'secret' : "10xauthsecret",
+    // 'db':"mongodb://localhost:27017/node-auth"
+    'db': "mongodb+srv://root:shubham@7785@cluster0.hmjlqif.mongodb.net/?retryWrites=true&w=majority"
+}
